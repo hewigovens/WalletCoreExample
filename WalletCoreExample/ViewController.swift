f34f81fbc9902e33d84171040dc52029fb8474da
hewigovens <360470+hewigovens@users.noreply.github.com>
2019-10-29 15:16:56 +0800

    Initial Commit

---
+++ WalletCoreExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WalletCoreExample
+//
+//  Created by Tao X on 10/29/19.
+//  Copyright © 2019 Tao X. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
